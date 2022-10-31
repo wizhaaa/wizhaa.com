e78abfa14cf6d5cbde2a71b724f07524a2bb79be
Will Zhang <wz282@cornell.edu>
2022-10-31 01:03:46 -0400

    add resume

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {Route, Routes} from "react-router";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
+import Resume from "./pages/Resume";
 import NoPage from "./pages/NoPage";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
+          <Route path="resume" element={<Resume />} />
 
           <Route path="/*" element={<NoPage />} />
         </Routes>
