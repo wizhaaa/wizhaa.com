410e105243518f1b41b35ba186233aa93854ca87
Will Zhang <wz282@cornell.edu>
2022-10-31 01:08:20 -0400

    add resume page

---
+++ src/pages/Resume.tsx
@@ -0,0 +1,20 @@
+// import Nav from "../components/Nav";
+// import Waves from "../components/Waves";
+// import Footer from "../components/Footer";
+
+// import {Document, Page} from "react-pdf";
+// import resume from "../assets/resume.pdf";
+
+const NoPage = () => {
+  return (
+    <iframe
+      title="resume"
+      src="https://drive.google.com/file/d/1LgjlD8VxIRxObOViEdRPD9nHIkQ1LnLW/preview"
+      width="100%"
+      height="1500px"
+      allow="autoplay"
+    ></iframe>
+  );
+};
+
+export default NoPage;
