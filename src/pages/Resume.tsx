383615d990134cfcbadd6a3f7066ff48cbd431ac
Will Zhang <wz282@cornell.edu>
2022-11-29 16:18:11 -0500

    resume page inside imbed

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,19 +1,30 @@
-// import Nav from "../components/Nav";
-// import Waves from "../components/Waves";
-// import Footer from "../components/Footer";
+import Nav from "../components/Nav";
+import Waves from "../components/Waves";
+import Footer from "../components/Footer";
 
-// import {Document, Page} from "react-pdf";
-// import resume from "../assets/resume.pdf";
+import {Document, Page} from "react-pdf";
+import resume from "../assets/resume.pdf";
 
 const NoPage = () => {
   return (
-    <iframe
-      title="resume"
-      src="https://drive.google.com/file/d/1LgjlD8VxIRxObOViEdRPD9nHIkQ1LnLW/preview"
-      width="100%"
-      height="1500px"
-      allow="autoplay"
-    ></iframe>
+    <div className="page">
+      <div className="half">
+        <Nav />
+      </div>
+      <Waves />
+      <div className="milk">
+        <h1> Resume </h1>
+        <iframe
+          title="resume"
+          src="https://drive.google.com/file/d/1LgjlD8VxIRxObOViEdRPD9nHIkQ1LnLW/preview"
+          width="90%"
+          height="1000px"
+          allow="autoplay"
+        ></iframe>
+      </div>
+
+      <Footer />
+    </div>
   );
 };
 
