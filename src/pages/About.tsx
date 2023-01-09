33aab577578e6e91299d6678a041d92e9bdd6ce5
Will Zhang <wz282@cornell.edu>
2023-01-09 18:56:37 -0500

    only 1 anim on about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -18,34 +18,33 @@ const About = () => {
         <p className="headline">
           First-gen coder having fun making projects on the web.
         </p>
-        <div className="animation">
-          <div className="about-body">
-            <div className="about-content">
-              <p>
-                Currently, I'm attending Cornell University as a computer
-                science major while serving as the{" "}
-                <em>Technical Project Manager </em> for Design at Cornell within
-                Design Tech & Initiative, acting <em> Vice President </em>
-                of the Association of Computer Science Undergraduates, a{" "}
-                <em>Big </em>
-                in the Chinese Student Association, and a <em>brother </em>in
-                Alpha Phi Omega.
-              </p>
-              <p>
-                As for development, I have real project experience with
-                git/Github work flows, <em> collaborating</em> with project
-                managers, developers, designers on Figma, Notion, Jira and more,
-                working with large codebases, setting up well-structured and
-                organized projects,
-                <em> deployment</em> on Virtual Machines like Digital Ocean,
-                setting up and interacting with databases like MongoDB and
-                Firebase,
-                <em> web frameworks </em>
-                like Express, React, Next, MUI and more.
-              </p>
-            </div>
-            <img className="me2-img" src="/me2.jpeg" alt="me" />
+
+        <div className="about-body">
+          <div className="about-content">
+            <p>
+              Currently, I'm attending Cornell University as a computer science
+              major while serving as the <em>Technical Project Manager </em> for
+              Design at Cornell within Design Tech & Initiative, acting{" "}
+              <em> Vice President </em>
+              of the Association of Computer Science Undergraduates, a{" "}
+              <em>Big </em>
+              in the Chinese Student Association, and a <em>brother </em>in
+              Alpha Phi Omega.
+            </p>
+            <p>
+              As for development, I have real project experience with git/Github
+              work flows, <em> collaborating</em> with project managers,
+              developers, designers on Figma, Notion, Jira and more, working
+              with large codebases, setting up well-structured and organized
+              projects,
+              <em> deployment</em> on Virtual Machines like Digital Ocean,
+              setting up and interacting with databases like MongoDB and
+              Firebase,
+              <em> web frameworks </em>
+              like Express, React, Next, MUI and more.
+            </p>
           </div>
+          <img className="me2-img" src="/me2.jpeg" alt="me" />
         </div>
 
         <WatchList />
