090fa0fdd150255f49f9341913761cd1f6c98244
Will Zhang <wz282@cornell.edu>
2024-01-18 17:50:54 -0500

    delete extras on resume pg

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,29 +1,7 @@
-import Waves from "../components/Waves";
 import Footer from "../components/Footer";
 
-// import {Document, Page} from "react-pdf";
 import resume from "../assets/resume.pdf";
 import NavBar from "../components/NavBar";
-
-const Resume1 = () => {
-  return (
-    <div className="page">
-      <Waves />
-      <div className="milk">
-        <h1 className="about-h"> Resume </h1>
-        <p> Last Updated: 08/26/2023 </p>
-        <br />
-        <iframe
-          title="resume"
-          id="resume"
-          src={resume}
-          frameBorder="0"
-        ></iframe>
-      </div>
-      <Footer />
-    </div>
-  );
-};
 
 const Resume = () => {
   return (
